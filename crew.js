7a3ad6f69f04b76a53891e0e806cb6ed99e04e9b
Patrick Spencer <spencerp@flip1.engr.oregonstate.edu>
2017-11-20 13:36:28 -0800

    fixed update function

--- crew.js
+++ crew.js
@@ -79,7 +79,7 @@ module.exports = function(){
         var mysql = req.app.get('mysql');
         var sql = "INSERT INTO crew_member (fname, lname, crewbase, role) VALUES (?,?,?,?)";
         var inserts = [req.body.fname, req.body.lname, req.body.crewMemberBase, req.body.position];
-        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+	sql = mysql.pool.query(sql,inserts,function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
@@ -95,7 +95,7 @@ module.exports = function(){
         var mysql = req.app.get('mysql');
         var sql = "UPDATE crew_member SET fname=?, lname=?, crewbase=?, role=? WHERE id=?";
         var inserts = [req.body.fname, req.body.lname, req.body.crewbase, req.body.role, req.params.id];
-        sql = mysql.pool.query(sql,inserts,function(error, results, fields){
+	sql = mysql.pool.query(sql,inserts,function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
